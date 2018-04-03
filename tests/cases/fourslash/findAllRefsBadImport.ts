e294cbea4e12bd21eb350c5533229812b9990a5c
Sverre Johansen <svjohan@microsoft.com>
2018-04-03 12:03:33 +0200

    Removing tests/ folder. (failing on Windows CI)

--- tests/cases/fourslash/findAllRefsBadImport.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path='fourslash.ts'/>
-
-////import { [|ab|] as [|cd|] } from "doesNotExist";
-
-const [r0, r1]  = test.ranges();
-verify.referencesOf(r0, [r1]);
-verify.referencesOf(r1, [r1]);
