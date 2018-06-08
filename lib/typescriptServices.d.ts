66e4aff88dfbddb63e9e59eebb21cc7e4735bc69
Sverre Johansen <svjohan@microsoft.com>
2018-06-08 11:02:31 +0200

    Updated lib/ with `$ npm run jake LKG`
    npm install
    npm run jake LKG

--- lib/typescriptServices.d.ts
+++ lib/typescriptServices.d.ts
@@ -2446,6 +2446,7 @@ declare namespace ts {
         jsxFactory?: string;
         composite?: boolean;
         removeComments?: boolean;
+        resolutionPlatforms?: string[];
         rootDir?: string;
         rootDirs?: string[];
         skipLibCheck?: boolean;
