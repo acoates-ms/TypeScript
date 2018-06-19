3bab18c3f5d862e370b0efaf9255a846ff32d358
Sverre Johansen <svjohan@microsoft.com>
2018-06-19 12:42:55 +0200

    Removed tests/ folder to fix CI (long paths on win32)

--- tests/cases/fourslash/completionImportMeta.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path='fourslash.ts' />
-
-////import./**/
-
-goTo.marker("");
-verify.completionListContains("meta");
--- tests/projects/sample1/tests/index.ts
+++
@@ -1,5 +0,0 @@
-import * as c from '../core/index';
-import * as logic from '../logic/index';
-
-c.leftPad("", 10);
-logic.getSecondsInDay();
