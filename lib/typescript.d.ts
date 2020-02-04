530fcecedcd1ddb8a7dcdc0958a1e06e348dcb09
Sverre Johansen <svjohan@microsoft.com>
2020-02-04 19:46:12 +0100

    Upgraded TypeScript to 3.7.5-fix.0

--- lib/typescript.d.ts
+++ lib/typescript.d.ts
@@ -2637,6 +2637,7 @@ declare namespace ts {
         incremental?: boolean;
         tsBuildInfoFile?: string;
         removeComments?: boolean;
+        resolutionPlatforms?: string[];
         rootDir?: string;
         rootDirs?: string[];
         skipLibCheck?: boolean;
